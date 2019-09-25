0368862430f16c5bc42b6d77129eb60702ce0802
Leo <leoamby@gmail.com>
2019-09-25 15:18:03 +0300

    include updated files

--- app/models.py
+++ app/models.py
@@ -4,8 +4,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-    __tablename__="users"#
-    id = d
+    __tablename__="users"
+    id = db.Column(db.Integer, primary)
 
 class Project(db.Model):
     __tablename__="projects"
