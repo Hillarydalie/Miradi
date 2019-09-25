1983df33200eb4d7abdf532b480173179154fa6e
Leo <leoamby@gmail.com>
2019-09-25 16:01:48 +0300

    create the comment model

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique = True)
     password = db.Column(db.String(255))
     image =db.Column(db.Photo)
+    comments = db.relationship('Comment', backref='user', lazy='dynamic')
+    projects = db.relatiships('Project', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
@@ -19,8 +21,14 @@ class Project(db.Model):
     name = db.Column(db.String(255), unique = True)
     projectTimeline = db.Column(db.String(255))
     image = db.Column(db.Photo)
-    description = db.Column(db.TextField)
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
     
 
 class Comment(db.Model):
     __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.DateTime, default=datetime.time)
+    report = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    project_id = db.Colum(db.Integer, db.ForeignKey('projects.id'), nullable = False)
