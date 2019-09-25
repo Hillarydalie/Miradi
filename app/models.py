34b6e108f7067c046fc6f7fab9a4e56718475e05
Leo <leoamby@gmail.com>
2019-09-25 15:27:25 +0300

    create the user models

--- app/models.py
+++ app/models.py
@@ -5,7 +5,10 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     __tablename__="users"
-    id = db.Column(db.Integer, primary)
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(255), unique = True)
+    password = db.Column(db.String(255))
+    image =db.Column(db.Photo)
 
 class Project(db.Model):
     __tablename__="projects"
