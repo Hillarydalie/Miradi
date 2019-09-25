5ef9e6e20cf0533699872f826b56e0c4b1859792
Leo <leoamby@gmail.com>
2019-09-25 16:25:01 +0300

    validate critical columns by calling the nullable=False syntax

--- app/models.py
+++ app/models.py
@@ -6,8 +6,9 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     __tablename__="users"
     id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique = True)
-    password = db.Column(db.String(255))
+    password = db.Column(db.String(255), nullable=False)
     image =db.Column(db.Photo)
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
     projects = db.relatiships('Project', backref='user', lazy='dynamic')
@@ -17,18 +18,19 @@ class User(UserMixin, db.Model):
 
 class Project(db.Model):
     __tablename__="projects"
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255), unique = True)
-    projectTimeline = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key = False)
+    name = db.Column(db.String(255), unique = False)
+    projectTimeline = db.Column(db.String(255), nullable=False)
     image = db.Column(db.Photo)
-    description = db.Column(db.Text)
+    description = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    comments = db.relationship('Comment', backref='project', lazy='dynamic')
     
 
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     date = db.Column(db.DateTime, default=datetime.time)
-    report = db.Column(db.Text)
+    report = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
     project_id = db.Colum(db.Integer, db.ForeignKey('projects.id'), nullable = False)
