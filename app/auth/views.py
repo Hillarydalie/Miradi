738d2f239f0666f5342be0f1b22ea57306d8506c
MungaiKeren <wambukeren@gmail.com>
2019-09-25 16:42:49 +0300

    This commit creates the signup route, and logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,6 +27,38 @@ def login():
 @auth.route('/authentication/sign-up',methods=['GET','POST'])
 def signup():
 
+    if request.method=='POST':
+        form = request.form
+        username = form.get('username')
+        email = form.get('email')
+        password = form.get('password')
+        confirm_password = form.get('confirm_password')
 
+        if username==None or password==None or email==None or confirm_password==None:
+            error = "Fill in the fields"
+            return render_template('signup.html',error=error)
+        
+        if " " in username:
+            error="Username cannot be empty"
+            return render_template('signup.html',error=error)
 
+        if password != confirm_password:
+            error = "passwords do not match"
+            return render_template('signup.html',error=error)
+        else :
+            user = User.query.filter_by(username=username).first()
+            if user != None:
+                error = "user with that username already exists"
+                return render_template('signup.html',error=error)
+            user = User(username=username,email=email)
+            user.set_password(password)
+            user.save()
+            return redirect(url_for('auth.login'))
+        print(username)
 
+    return render_template('signup.html')
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
