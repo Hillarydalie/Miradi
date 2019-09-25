8b9e84bcab73d33bac467296112bdcf895ebc1d8
Leo <leoamby@gmail.com>
2019-09-25 16:45:13 +0300

    connect models with the authentifications and user loader

--- app/models.py
+++ app/models.py
@@ -13,10 +13,27 @@ class User(UserMixin, db.Model):
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
     projects = db.relatiships('Project', backref='user', lazy='dynamic')
 
-    def save
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def set_password(self, password):
+        password_hash = generate_password_hash(password)
+        self.password = password_hash
+
+    def check_password(self,password):
+        return check_password_hash (self.password, password)
 
     def __repr__(self):
         return f'User {self.username}'
+
+    @login_manager.user_loader
+    def user_loader(user_id):
+        return User.query.get(user_id)
 
 class Project(db.Model):
     __tablename__="projects"
