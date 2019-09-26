e68f46d380acd0499f020fa8956eda4abe848e79
IreneMercy <irenemercy700@gmail.com>
2019-09-26 14:59:11 +0300

    Style background atructure of  templates.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config:
-  
+
     DEBUG="True"
     SECRET_KEY='jklsjgkljskljgiorjiosnklfiowpnbriorbo'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     UPLOAD_PHOTOS_DEST = 'app/static/images'
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://hillarydalie:password@localhost:5432/miradi'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://joozao:12345@localhost:5432/miradi'
 
 
 class DevConfig(Config):
