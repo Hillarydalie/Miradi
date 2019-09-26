6358729019b5191e45d4c508333950e2e5a76100
hillarydalie <hidalie@gmail.com>
2019-09-26 10:42:41 +0300

    Solve errors from form submission

--- app/main/views.py
+++ app/main/views.py
@@ -16,13 +16,13 @@ def projects():
         form = request.form
         name = form.get('name')
         progress = form.get('progress')
-        period = form.get('period')
-        image = form .get('image')
+        projectTimeline = form.get('projecttimeline')
+        project_photo = form .get('project_photo')
         description = form.get('description')
-        if name==None or progress==None or period==None or image==None or description==None:
+        if name==None or progress==None or projectTimeline==None or project_photo==None or description==None:
             error = "Kindly fill all fields to continue"
             return render_template('projects.html', error=error)
-        project = Projects(name=name,progress=progress,period=period,image=image,description=description, user_id=current_user.id)
+        project = Project(name=name,progress=progress,projectTimeline=projectTimeline,project_photo=project_photo,description=description, user_id=current_user.id)
         project.save()
         return redirect(url_for('main.index'))
     return render_template('projects.html')
